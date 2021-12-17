4fbf3ae5a28903dac0dc566215ab8b3a119415cf
Anıl Orhan <anilorhan-@hotmail.com>
2021-12-17 05:05:37 +0300

    05:05 2

---
+++ Core/Utilities/Security/Encryption/SecurityKeyHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Encryption
+{
+    public  class SecurityKeyHelper
+    {
+        public static SecurityKey CreateSecurityKey(string securityKey) 
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));  
+            //şifreyi byte[] haline getiriyor.
+            //Simetrik Güvenlik Anahtarına Çeviriyor
+            //JWT'nin ihtiyaç duyduğu formata dönüştürüyor.
+        
+        }
+
+
+    }
+}
